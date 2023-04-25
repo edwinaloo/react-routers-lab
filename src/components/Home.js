ff6c14add252e5fa51fde130d36debf6f466016c
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-25 09:10:08 +0300

    add a picture to home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+      <p>Welcome to our movies app!</p>
+      <img src="pics/silent.jpeg" alt="Example Image" />
     </div>
   );
 }
