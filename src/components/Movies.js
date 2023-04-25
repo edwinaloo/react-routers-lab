15d04f6bd07ac964b093c0976f4c7d6f44adb303
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-25 08:57:37 +0300

    correct movies.js fix

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,24 +1,7 @@
 import React from "react";
+import { movies } from "../data";
 
 function Movies() {
-  const movies = [
-    {
-      title: "Movie 1",
-      time: "1h 30m",
-      genres: ["Comedy", "Drama"]
-    },
-    {
-      title: "Movie 2",
-      time: "1h 45m",
-      genres: ["Action", "Adventure"]
-    },
-    {
-      title: "Movie 3",
-      time: "2h 15m",
-      genres: ["Horror", "Thriller"]
-    }
-  ];
-
   return (
     <div>
       <h1>Movies Page</h1>
@@ -26,6 +9,7 @@ function Movies() {
         <div key={movie.title}>
           <h2>{movie.title}</h2>
           <p>Time: {movie.time}</p>
+          <p>Metascore: {movie.metascore}</p>
           <ul>
             {movie.genres.map((genre) => (
               <li key={genre}>{genre}</li>
@@ -38,4 +22,3 @@ function Movies() {
 }
 
 export default Movies;
-
