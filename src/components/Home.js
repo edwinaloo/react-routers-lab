a516458d133dfe9b2d5484133c6bf3a3dd645637
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-25 09:11:13 +0300

    home.js fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,10 @@ function Home() {
     <div>
       <h1>Home Page</h1>
       <p>Welcome to our movies app!</p>
-      <img src="pics/silent.jpeg" alt="Example Image" />
+      <img src="pics/silent.jpeg" alt="Example Image" style={{width: '50%'}} />
     </div>
   );
 }
 
 export default Home;
+
